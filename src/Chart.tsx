d39b3b7d0ad9eda883ac5e3a582bed637fa54744
Morten Lohne <lohnemorten@gmail.com>
2021-10-08 01:56:06 +0200

    Successfully fetch data from the api

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -2,6 +2,47 @@ import React from "react";
 import { LineChart, Line, XAxis, Tooltip, CartesianGrid } from 'recharts'
 
 class Chart extends React.Component {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            data: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://visningsrom.stacc.com/dd_server_laaneberegning/rest/laaneberegning/v1/nedbetalingsplan", 
+        { method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "laanebelop": 2000000,
+                "nominellRente": 3,
+                "terminGebyr":30,
+                "utlopsDato":"2045-01-01",
+                "saldoDato":"2020-01-01",
+                "datoForsteInnbetaling":"2020-02-01",
+                "ukjentVerdi":"TERMINBELOP"
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
+        /*
+        .then((result) => {
+            this.setState({
+                isLoaded: true,
+                data: result.nedbetalingsplan
+            })
+        })
+        */
+    }
+
     render() {
         const data = [
             {name: 'Page A', uv: 400, pv: 2400, amt: 2400},
