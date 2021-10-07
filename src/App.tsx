ac7c405c373b265f4768c573b87132bce416160e
Morten Lohne <lohnemorten@gmail.com>
2021-10-07 17:39:41 +0200

    Show basic chart using recharts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { LineChart, Line, XAxis, Tooltip, CartesianGrid } from 'recharts'
 
 function App() {
+  const data = [
+    {name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+    {name: 'Page B', uv: 600, pv: 2200, amt: 2200},
+    {name: 'Page C', uv: 200, pv: 1600, amt: 2600}];
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +24,18 @@ function App() {
           Learn React
         </a>
       </header>
+      <LineChart
+        width={400}
+        height={400}
+        data={data}
+        margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+      >
+        <XAxis dataKey="name" />
+        <Tooltip />
+        <CartesianGrid stroke="#f5f5f5" />
+        <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+        <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+      </LineChart>
     </div>
   );
 }
