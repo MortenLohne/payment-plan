29ec7ee40789ec63bfa8ca4ab6e892bb5abc745b
Morten Lohne <lohnemorten@gmail.com>
2021-10-10 01:59:59 +0200

    Add ChartData state to root component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,25 @@ import DataEntry from './DataEntry';
 import React from 'react'
 import ccue from './ccue_logo.png'
 
-class App extends React.Component {
+type ChartData = {loanAmount: number, expirationYear: number, interest: number}
+
+class App extends React.Component<{}, ChartData> {
+  constructor(props: {}) {
+    super(props);
+
+    this.handleChartDataChange = this.handleChartDataChange.bind(this);
+
+    this.state = {
+      loanAmount: 3000000,
+      expirationYear: 2046,
+      interest: 3.0,
+    }
+  }
+
+  handleChartDataChange(chartData: ChartData) {
+    this.setState(chartData)
+  }
+
   render() {
     return (
       <div className="App">
