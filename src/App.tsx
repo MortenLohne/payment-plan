ec236331381e25faf269ab2cf408bb425c18cded
Morten Lohne <lohnemorten@gmail.com>
2021-10-09 20:45:40 +0200

    Add ccue logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css';
 import Chart from './Chart'
 import DataEntry from './DataEntry';
 import React from 'react'
+import ccue from './ccue_logo.png'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <img src={ccue} alt="logo"/>
         <DataEntry/>
         <Chart loanAmount={3000000} expirationYear={2046} interest={3.0}/>
       </div>
