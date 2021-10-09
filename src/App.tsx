1c066e268af463caf63c7783c6fd1ab4438311d2
Morten Lohne <lohnemorten@gmail.com>
2021-10-09 17:18:37 +0200

    Turn App into a class

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import './App.css';
 import Chart from './Chart'
 import DataEntry from './DataEntry';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <DataEntry/>
-      <Chart/>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <DataEntry/>
+        <Chart/>
+      </div>
+    );
 }
+}
+
+
 
 export default App;
